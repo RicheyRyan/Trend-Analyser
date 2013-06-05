321159a649147c219fd289481e0aba4b0022fd11
Richey Ryan <richeyryan@gmail.com>
2013-06-05 20:33:00 +0100

    Setup to be doc page.

--- generate-docs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-lein marg ./src/trend_analyser/handler.clj ./src/trend_analyser/tweets.clj ./src/trend_analyser/html.clj ./src/trend_analyser/data.clj ./src/trend_analyser/scheduler.clj ./project.clj
-ditto ./docs/uberdoc.html ./public
